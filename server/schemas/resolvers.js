3ffd6c44f6b3f030660ac82d083174d6e427ded7
Brian T. <bptiburcio@gmail.com>
2024-08-06 20:12:03 -0400

    Add typedefs for resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,47 @@
+const typeDefs = `
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+        bookCount: Int
+    }
+
+    type Query {
+        getUser(id: ID!): User
+        getUsers: [User]
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): User
+        login(email: String!, password: String!): Auth
+        saveBook(book: BookInput!): User
+        removeBook(bookId: String!): User
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Auth {
+        token: String!
+        user: User
+    }
+`;
+
+module.exports = typeDefs;
